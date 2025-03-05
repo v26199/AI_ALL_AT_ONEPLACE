fa1ae2f1cd04d0a5f541bbf17c0d406c77a290c2
Vishal Patel <58775674+v26199@users.noreply.github.com>
2025-03-04 20:14:08 -0500

    Delete ImageCaption directory

--- ImageCaption/blip-image-captioning-api/app/config.py
+++
@@ -1,9 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    blip_model_name: str = "Salesforce/blip-image-captioning-large"
-
-settings = Settings()
-
-# In config.py
-print(dir())  # See what symbols are available at the end of the file
